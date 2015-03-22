ce9dca6ae1e4f9771c0e0251a19bda7936dd1e2b
Artur Malabarba <bruce.connor.am@gmail.com>
2015-03-22 10:49:39 +0000

    Add commentary

--- bug-hunter.el
+++ bug-hunter.el
@@ -22,8 +22,39 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
-;;
+;; `bug-hunter' is an Emacs library that finds the source of a bug inside
+;; any given `.el' file.
+;; 
+;; 
+;; Usage Examples
+;; ==============
+;; 
+;;   1. If your Emacs init file signals an error during startup, but you
+;;      don’t know why, simply issue
+;;      ,----
+;;      | M-x bug-hunter-init-file RET RET
+;;      `----
+;;      and `bug-hunter' will find it for you.
+;; 
+;;   2. If Emacs starts up without errors but something is not working as
+;;      it should, invoke the same command, but give it in an assertion:
+;;      ,----
+;;      | M-x bug-hunter-init-file RET (test-the-misbehaving-feature) RET
+;;      `----
+;; 
+;;   3. You can also use `bug-hunter-file' to hunt in other files, or even
+;;      `bug-hunter-hunt' to search in a list of expressions.
+;; 
+;; 
+;; Installation
+;; ============
+;; 
+;;   It is part of Emacs and is available trough GNU Elpa. To install,
+;;   issue
+;; 
+;;   ,----
+;;   | M-x package-install RET bug-hunter
+;;   `----
 
 ;;; Code:
 
